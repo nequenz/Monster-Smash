cfa493f4aa3122864d9eb26742f6906664df88cf
Slava <70316676+nequenz@users.noreply.github.com>
2023-01-25 19:33:43 +0300

    clean component realization

--- Assets/Code/NonMonoBehaviour/RootComponent.cs
+++ Assets/Code/NonMonoBehaviour/RootComponent.cs
@@ -2,7 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public delegate void ComponentDelegate<T>(T instance);
+
 public class RootComponent : MonoBehaviour
 {
-   
+    private List<ICleanComponent> _components = new();
+
+    private void Awake()
+    {
+        
+    }
+
+    protected void InitComponents() => _components.ForEach((component) => component.Init(this));
+
+    protected void UpdateComponent()
+    {
+        _components.ForEach((component) =>
+        {
+            if (component.IsEnabled)
+                component.Update();
+        });
+    }
+
+    protected void FixedUpdateComponent()
+    {
+        _components.ForEach((component) =>
+        {
+            if (component.IsEnabled)
+                component.FixedUpdate();
+        });
+    }
+
+    protected void AddComponent(ICleanComponent component) => _components.Add(component);
+
+    public T FindComponent<T>()  => (T)_components.Find((component) => component is T);
 }
