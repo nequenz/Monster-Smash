888a99ef38d922af41a7f57e57b5c382a71b01e6
Slava <70316676+nequenz@users.noreply.github.com>
2023-01-25 20:51:55 +0300

    web gl inits

---
+++ Assets/WebGamesTemplate/Scripts/MenuDisplay/MenuDisplay.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+
+public class MenuDisplay : MonoBehaviour
+{
+    [SerializeField] private LeaderBoardButton _leaderBoardButton;
+    [SerializeField] private InviteFriendsButton _inviteFriendsButton;
+
+    private void Start()
+    {
+        Show();
+    }
+
+    public void Show()
+    {
+    #if YANDEX_GAMES || UNITY_EDITOR
+            _leaderBoardButton.gameObject.SetActive(true);
+    #endif
+
+    #if VK_GAMES
+            if (Application.isMobilePlatform)
+            {
+                _leaderBoardButton.gameObject.SetActive(true);
+            }
+            _inviteFriendsButton.gameObject.SetActive(true);
+    #endif
+    }
+
+    public void Hide()
+    {
+        _leaderBoardButton.gameObject.SetActive(false);
+
+#if VK_GAMES
+        _inviteFriendsButton.gameObject.SetActive(false);
+#endif
+    }
+}
