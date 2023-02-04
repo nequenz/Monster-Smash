2489a112c6342771b980c05a5088eb57b1b90a14
Slava <70316676+nequenz@users.noreply.github.com>
2023-02-04 16:32:55 +0300

    *

--- Assets/Code/Voxel/IVoxelData.cs
+++ Assets/Code/Voxel/IVoxelData.cs
@@ -1,17 +1,19 @@
 ﻿using UnityEngine;
 using System;
 
-public interface IVoxelBody
+public interface IVoxelData
 {
     public event Action<Vector3> Collided;
     public event Action<Vector3> Changed;
 
 
     public bool CanBeReallocated { get; }
+    public Vector3 Size { get; }
+
 
     public void Allocate(Vector3Int size);
 
     public void SetVoxel(Vector3Int position, byte value);
 
-    public void GetVoxel(Vector3Int position);
+    public byte GetVoxel(Vector3Int position);
 }
---
+++ Assets/Code/Voxel/VoxelData.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System;
+
+public class VoxelData : IVoxelData
+{
+    private bool _canBeReallocated = false;
+    private byte[,,] _voxel;
+    private Vector3 _size;
+
+
+    public event Action<Vector3> Collided;
+    public event Action<Vector3> Changed;
+
+
+    public bool CanBeReallocated => _canBeReallocated;
+    public Vector3 Size => _size;
+
+
+    private bool IsAccessValid(Vector3 position)
+    {
+        bool zeroCheck = position.x >= 0 && position.y >= 0 && position.z >= 0;
+        bool sizeCheck = position.x < _size.x && position.y < _size.y && position.z < _size.z;
+
+        return zeroCheck && sizeCheck;
+    }
+
+    public void Allocate(Vector3Int size)
+    {
+        if(size.x < 0 || size.y < 0 || size.z < 0)
+            throw new ArgumentOutOfRangeException("Wrong params of allocation size");
+
+        _voxel = new byte[size.x, size.y, size.z];
+        _size = size;
+    }
+
+    public byte GetVoxel(Vector3Int position)
+    {
+        if (IsAccessValid(position))
+            return _voxel[position.x, position.y, position.z];
+
+        return 0x0;
+    }
+
+    public void SetVoxel(Vector3Int position, byte value)
+    {
+        if (IsAccessValid(position))
+            _voxel[position.x, position.y, position.z] = value;
+    }
+}
